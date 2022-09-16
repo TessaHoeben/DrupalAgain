a0ae8dc37798d154fdae706f148303a56fbd9314
Tessa <anjarno.tessa@gmail.com>
2022-09-16 15:08:35 +0200

    setup

---
+++ web/themes/react_example_theme/js/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// # Example 1: Simple "Hello, World" code
+ReactDOM.render(
+  <h1>pleeasse work</h1>,
+  document.getElementById('react-app')
+);
+console.log("this works");
